43e14609610bbb3f23dbf4fe9a542a14b3442147
andybraren <andybraren@gmail.com>
2017-03-20 14:59:35 -0400

    New modal structure
    Modals are opened based on the property of data-modal within a clicked element. This is more scalable than manually adding click listeners to every unique element.

--- site/snippets/modals.php
+++ site/snippets/modals.php
@@ -1,3 +1,138 @@
+<div id="modals">
+  
+  <div id="modal-backdrop"></div>
+  
+  <?php // DELETE ?>
+  <div id="modal-delete" class="modal">
+    
+    <div class="modal-title">
+      <h2>Delete this page?</h2>
+      <button type="button" aria-label="close" data-action="close"></button>
+    </div>
+    
+    <div class="modal-content">
+      <span>This entire page and all of its content will be deleted.</span>
+    </div>
+    
+    <div class="modal-options">
+      <button type="button" data-action="close" class="button fullwidth silver">Never mind</button>
+      <button type="button" data-action="delete" class="button fullwidth red">Delete</button>
+    </div>
+    
+  </div>
+  
+  <?php // LOG IN ?>
+  <div id="modal-login" class="modal">
+    
+    <div class="modal-title">
+      <h2>Log in</h2>
+      <button type="button" aria-label="close" data-action="close"></button>
+    </div>
+    
+    <div class="modal-content">
+      <form id="login" action="<?php echo $page->url() . '/login' ?>" method="post">
+        
+        <?php if (param('login') == 'failed'): ?>
+          <div class="red highlight card-join">
+            <span>Login failed. Try again, or contact <a href="&#109;&#97;ilto&#58;and%79&#98;rare%&#54;&#69;&#64;g&#109;a%&#54;9l&#46;&#99;%6&#70;m?subject=Website login issue">Andy</a> for help.</span>
+          </div>
+        <?php endif ?>
+        
+        <div>
+          <input type="text" name="username" pattern="^[a-zA-Z0-9]{3,20}$|[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[a-z]{2,3}$" required autofocus> <?php // Only letters and numbers, between 3 and 20 ?>
+          <label for="username">Username (or email)</label>
+        </div>
+        
+        <div>
+          <input type="password" name="password" pattern=".{4,}" required> <?php // At least 4 characters?>
+          <label for="password">Password</label>
+        </div>
+        
+        <span id="button-forgot" data-modal="forgot">I forgot my password</span>
+        
+      </form>
+    </div>
+    
+    <div class="modal-options">
+      <button type="button" data-action="close" class="button fullwidth silver">Never mind</button>
+      <button type="submit" data-action="login" class="button fullwidth" form="login">Log in</button>
+    </div>
+    
+  </div>
+  
+  <?php // FORGOT PASSWORD ?>
+  <div id="modal-forgot" class="modal">
+      
+    <div class="modal-title">
+      <h2>Password Reset</h2>
+    </div>
+    
+    <div class="modal-content">
+      <?php if (!param('forgot')): ?>
+        <span>Enter the non-Tufts email address you signed up with and password reset instructions will be sent to you.</span>
+        
+        <form action="forgot" method="post">
+          <div>
+            <input type="text" name="email" required>
+            <label for="email">Email address</label>
+          </div>
+          <div>
+            <input type="submit" class="button fullwidth" name="forgot" value="Send reset email">
+          </div>
+        </form>
+      <?php endif ?>
+    </div>
+    
+    <?php if (param('forgot') == 'success'): ?>
+      <div class="green highlight card-join">
+        <span>An email has been sent to your inbox. Click the link it contains to reset your password.</span>
+      </div>
+    <?php elseif (param('forgot') == 'failed'): ?>
+        <div class="red highlight card-join">
+          <span>Password reset failed. Try again, or contact <a href="&#109;&#97;ilto&#58;and%79&#98;rare%&#54;&#69;&#64;g&#109;a%&#54;9l&#46;&#99;%6&#70;m?subject=Website login issue">Andy</a> for help.</span>
+        </div>
+    <?php endif ?>
+      
+  </div>
+  
+  
+  
+  
+  
+  
+  
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 <div class="modals">
   
   <div id="modal-reading">

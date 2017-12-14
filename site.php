b728c899c23915158f96d670f4c6602d3636b78d
andybraren <andybraren@gmail.com>
2017-12-13 19:37:42 -0500

    Changed ‘users’ folder location
    Because example.com/users would be a common location for a user directory, the users folder containing account info and avatars needed to be moved elsewhere. For now it’s in a new top-level ‘acorn’ folder. Ideally it would be within the site folder, but that would make “upgrading” site site manually more difficult unless the entirety of Acorn were a series of Kirby plugins.

--- site.php
+++ site.php
@@ -7,14 +7,15 @@ $kirby   = kirby();
 //$kirby->roots->avatars = __DIR__ . DS . 'content' . DS . 'users' . DS . 'avatars' . DS . $domain;
 //$kirby->urls->avatars  = $kirby->urls()->index() . '/content/users/avatars';
 
-$kirby->roots->accounts = $kirby->roots()->index() . DS . 'users' . DS . 'accounts'; // accounts moved to /accounts/accounts
-$kirby->roots->cache    = $kirby->roots()->index() . DS . 'cache' . DS . 'content';     // cache moved to /cache/content
+$kirby->roots->accounts = $kirby->roots()->index() . DS . 'acorn' . DS . 'users' . DS . 'accounts';    // accounts moved to /acorn/users
 
-$kirby->roots->avatars  = $kirby->roots()->index() . DS . 'users' . DS . 'avatars';
-$kirby->urls->avatars   = $kirby->urls()->index() . '/users/avatars';
+$kirby->roots->avatars  = $kirby->roots()->index() . DS . 'acorn' . DS . 'users' . DS . 'avatars';
+$kirby->urls->avatars   = $kirby->urls()->index() . DS . 'acorn' . DS . 'users' . DS . 'avatars';
 
 $kirby->roots->assets  = $kirby->roots()->index() . DS . 'site' . DS . 'assets';
 $kirby->urls->assets   = $kirby->urls()->index() . '/site/assets';
+
+$kirby->roots->cache    = $kirby->roots()->index() . DS . 'cache' . DS . 'content';     // cache moved to /cache/content
 
 //$kirby->roots->site = __DIR__ . DS . 'acorn';
 
